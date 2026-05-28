9553c55b05dfb6d17d44866778ccceb807c5dc17
SammyBot <sammybot@openclaw.attlocal.net>
2026-05-28 08:43:01 -0400

    [sai] switch from netlify to tailscale hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://broadsheet-news.netlify.app',
+  site: 'https://sammybots-macbook-pro.tail5e4166.ts.net:8443',
   trailingSlash: 'never',
   build: {
     format: 'directory',
